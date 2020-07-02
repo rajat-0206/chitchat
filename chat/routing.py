57d950901958e08e5d5e009cace5bc0db99f949f
Priyanshu Arora <priyanshu.18bcs1097@abes.ac.in>
2020-07-02 23:35:53 +0530

    ws changes consumer.py

--- chat/routing.py
+++ chat/routing.py
@@ -5,3 +5,8 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
+channel_routing = {
+    'websocket.connect': consumers.ChatConsumer.connect,
+    'websocket.receive': consumers.ChatConsumer.receive,
+    'websocket.disconnect': consumers.ChatConsumer.disconnect,
+}
