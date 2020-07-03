f669042395856eeb5dbeba6abd0104881d602945
Priyanshu Arora <priyanshu.18bcs1097@abes.ac.in>
2020-07-04 01:44:26 +0530

    msave admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import pic,dp
+from .models import pic,dp,message_save
 admin.site.register(pic)
 admin.site.register(dp)
+admin.site.register(message_save)
