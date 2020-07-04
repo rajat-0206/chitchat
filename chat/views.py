05c17488407399bc8e5176425fac4a234c1cf768
= <=>
2020-07-04 22:53:31 +0530

    notifinal

--- chat/views.py
+++ chat/views.py
@@ -266,6 +266,13 @@ def save_message(request):
         u_name1=name_by_id1.username
         name2=name_by_id2.first_name+" "+name_by_id2.last_name
         u_name2=name_by_id2.username
+        if(u_name2==request.session['username'])
+        {
+            recipient = u_name1
+        }
+        else{
+            recipient = u_name2
+        }
         if naam==name1:
             m_id1=name_by_id1.id
         else:
@@ -288,7 +295,7 @@ def save_message(request):
             new=lastmessage.objects.create(myid=name2,fid=name1,cus_id='https://itschitchat.herokuapp.com/chating/'+c_id_in,Uname=u_name2,f_id=m_id1,pic_url=pic.objects.get(user_id=m_id1).pic_url)
             new.save()
         header = {"Content-Type":"application/json; charset=utf-8","Authorization": "Basic NGQ1NDJmZmYtYjc2ZS00YTA5LThlZDMtYzA0MzQ3YTBhYjU1"}
-        payload = {"app_id":"56f464d8-5f40-479c-b005-7bbc1dff146d","include_external_user_ids":[u_name2],"contents":{"en":"You have recieved a new message from "+name1},"headings":{"en":"Chitchat"},"url":"https://itschitchat.herokuapp.com/chating/"+c_id_in,"chrome_web_icon":"https://itschitchat.pythonanywhere.com/media/media/"+u_name1+".jpg","chrome_web_badge":"https://itschitchat.herokuapp.com/static/images/icon-192x192.png"}
+        payload = {"app_id":"56f464d8-5f40-479c-b005-7bbc1dff146d","include_external_user_ids":[u_name2],"contents":{"en":"You have recieved a new message from "+naam},"headings":{"en":"Chitchat"},"url":"https://itschitchat.herokuapp.com/chating/"+c_id_in,"chrome_web_icon":"https://itschitchat.pythonanywhere.com/media/media/"+naam+".jpg","chrome_web_badge":"https://itschitchat.herokuapp.com/static/images/icon-192x192.png"}
         print(payload)
         req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
         print(req.status_code, req.reason,req.text,sep="----000----")
