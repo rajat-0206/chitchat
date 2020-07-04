2c3cf2885e535f73f9ad3b3a6277badd765c7316
= <=>
2020-07-04 22:18:40 +0530

    notitry10

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, '/template/chat/', 'serviceworker.js')
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'chat/static/script', 'serviceworker.js')
 
 
 # Quick-start development settings - unsuitable for production
