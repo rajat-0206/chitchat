3959d1c20111946f681c0a9ded27a7fe683517a0
= <=>
2020-07-05 17:49:14 +0530

    404 aded

--- chat/views.py
+++ chat/views.py
@@ -22,8 +22,8 @@ from .models import message_save,lastmessage
 global logged
 
 def handle404(request,exception):
-    return render(request,"404.html")
-    
+    return render(request,"chat/404.html")
+
 def manifest(request):
     return render(request,'chat/manifest.json')
 
