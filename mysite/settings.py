ea5356bf0f7ead896b100fe6d7bd9b4af17b8b76
Priyanshu Arora <priyanshu.18bcs1097@abes.ac.in>
2020-07-18 14:08:36 +0530

    videocall done

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'chat/static')
+#     ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'chat/static')
-    ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR,'chat/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'chat/static')
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
