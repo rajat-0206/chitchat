430087b0f37fcf4b33615a5371f7956952a04fa4
= <=>
2020-09-03 15:12:09 +0530

    not working

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'chat/static/script', 'servicew
 SECRET_KEY = 'xbpdeoc&ni-w84aa$mch6yuq$uhbw-$5!k7mxfmxlmxh(9xi%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['itschitchat.herokuapp.com']
 
 
